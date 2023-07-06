ac8ae157ca50be6a5104050c645a60dc521b5e24
echestnut22 <eric.chestnut232@gmail.com>
2023-07-06 10:38:53 -0600

    Code Updates

--- data/df_scraper.py
+++ data/df_scraper.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 
 
 #Get CSV of all pokemon with pokedex number, name, type(s), stats. Has megas and alterantive forms. No gigantimax 
+#Uncleaned version of dataframe. Cleaned version provided in repository as pokedex.csv 
+#Code for cleaning dataframe provided below in docstring 
 
 url = 'https://pokemondb.net/pokedex/all'
 response = requests.get(url)
@@ -32,3 +34,84 @@ with open('pokedex.csv', mode='w', newline='', encoding='utf-8') as csv_file:
 
 
 
+
+"""
+
+#Cleaning DataFrame
+#Removing name redundance for alternative forms
+
+df = pd.read_csv('C:/Users/ericc/.vscode/Jupyter/Pokemon/pokedex.csv')
+# rename the 'Pokedex Number' column to 'NDex'
+df = df.rename(columns={"Pokedex Number": "NDex"})
+#Have all NDex numbers have four digits (will be easier for matching PNGs later)
+df['NDex'] = df['NDex'].astype(str).str.zfill(4)
+
+alternative_forms = ["Mega", "Alolan", "Partner", "Galarian", "Hisuian"]
+
+# Create a regular expression pattern to match the preceding words
+pattern = r"\b\w+\b(?=\s+(?:" + "|".join(alternative_forms) + r"))"
+
+# Apply the lambda function to the "Name" column to remove preceding words
+df["Name"] = df["Name"].apply(lambda x: re.sub(pattern, "", x).strip())
+
+# Create a list of replacement rules
+replacement_rules = {
+    "Farfetch' Galarian Farfetch'd": "Galarian Farfetch'd",
+    "Mr.  Galarian Mr. Mime": "Galarian Mr. Mime",
+    "Kyogre Primal Kyogre":"Kyogre Primal",
+    "Groudon Primal Groudon": "Groudon Primal",
+    "Wooper Paldean Wooper":"Paladean Wooper",
+    "Rotom Heat Rotom": "Rotom Heat",
+    "Rotom Wash Rotom": "Rotom Wash",
+    "Rotom Frost Rotom": "Rotom Frost",
+    "Rotom Fan Rotom": "Rotom Fan",
+    "Rotom Mow Rotom": "Rotom Mow",
+    
+    "Kyurem White Kyurem": "Kyurem White",
+    "Kyurem Black Kyurem": "Kyurem Black",
+    "Greninja Ash-Greninja": "Ash Greninja",
+    
+    "Pumpkaboo Average Size": "Pumpkaboo",
+    
+    "Gourgeist Average Size": "Gourgeist",
+    
+    "Hoopa Hoopa Confined": "Hoopa Confined",
+    "Hoopa Hoopa Unbound": "Hoopa Unbound",
+    
+    "Type: Null": "Type Null",
+    
+    "Maushold Family of Four": "Maushold",
+    "Maushold Family of Three": "Maushold Three",
+    
+    "Palafin Zero Form": "Palafin",
+    
+    "Zygarde 50% Forme": "Zygarde",
+    
+    "Necrozma Ultra Necrozma": "Necrozma Ultra",
+    "Necrozma Dusk Mane Necrozma": "Necrozma Dusk Mane",
+    "Necrozma Dawn Wings Necrozma": "Necrozma Dawn Wings",
+    
+    "Ho-oh": "Ho-Oh",
+    
+    "Darmanitan Standard Mode": "Darmanitan",
+    "Galarian Standard Mode": "Galarian Darmanitan",
+    "Galarian Zen Mode": "Galarian Darmanitan Zen Mode",
+    
+} 
+
+# Apply the replacement rules to the "Name" column
+df["Name"] = df["Name"].replace(replacement_rules)
+
+#Remove Some pokemon from df 
+df = df[~df['Name'].isin(['Pumpkaboo Small Size', 'Pumpkaboo Large Size', 'Pumpkaboo Super Size', 'Gourgeist Small Size', 'Gourgeist Large Size', 'Gourgeist Super Size', 'Rockruff Own Tempo Rockruff', 'Eternatus Eternamax','Partner Eevee','Giratina Altered Forme'])]
+
+#Ensure pokemon of first form comes first in DataFrame. Important for hyperlinks
+def sort_group(group):
+    return group.sort_values(by='Name', key=lambda x: x.str.len())
+df = df.groupby('NDex', group_keys=False).apply(sort_group)
+
+#Make all NDex numbers have 4 digits.
+df['NDex'] = df['NDex'].astype(str).str.zfill(4)
+
+
+"""
--- data/image_naming.py
+++ data/image_naming.py
@@ -3,7 +3,6 @@
 dir_path = r''
 
 import os
-import re
 import pandas as pd
 
 #CHANGE NAMES OF IMAGES 
@@ -175,84 +174,5 @@ for filename in os.listdir(dir_path):
                 pass
 
 
-
-
-
-
-#Cleaning DataFrame
-#Removing name redundance for alternative formS
-
-df = pd.read_csv('C:/Users/ericc/.vscode/Jupyter/Pokemon/pokedex.csv')
-# rename the 'Pokedex Number' column to 'NDex'
-df = df.rename(columns={"Pokedex Number": "NDex"})
-#Have all NDex numbers have four digits (will be easier for matching PNGs later)
-df['NDex'] = df['NDex'].astype(str).str.zfill(4)
-
-alternative_forms = ["Mega", "Alolan", "Partner", "Galarian", "Hisuian"]
-
-# Create a regular expression pattern to match the preceding words
-pattern = r"\b\w+\b(?=\s+(?:" + "|".join(alternative_forms) + r"))"
-
-# Apply the lambda function to the "Name" column to remove preceding words
-df["Name"] = df["Name"].apply(lambda x: re.sub(pattern, "", x).strip())
-
-# Create a list of replacement rules
-replacement_rules = {
-    "Farfetch' Galarian Farfetch'd": "Galarian Farfetch'd",
-    "Mr.  Galarian Mr. Mime": "Galarian Mr. Mime",
-    "Kyogre Primal Kyogre":"Kyogre Primal",
-    "Groudon Primal Groudon": "Groudon Primal",
-    "Wooper Paldean Wooper":"Paladean Wooper",
-    "Rotom Heat Rotom": "Rotom Heat",
-    "Rotom Wash Rotom": "Rotom Wash",
-    "Rotom Frost Rotom": "Rotom Frost",
-    "Rotom Fan Rotom": "Rotom Fan",
-    "Rotom Mow Rotom": "Rotom Mow",
-    
-    "Kyurem White Kyurem": "Kyurem White",
-    "Kyurem Black Kyurem": "Kyurem Black",
-    "Greninja Ash-Greninja": "Ash Greninja",
-    
-    "Pumpkaboo Average Size": "Pumpkaboo",
-    
-    "Gourgeist Average Size": "Gourgeist",
-    
-    "Hoopa Hoopa Confined": "Hoopa Confined",
-    "Hoopa Hoopa Unbound": "Hoopa Unbound",
-    
-    "Type: Null": "Type Null",
-    
-    "Maushold Family of Four": "Maushold",
-    "Maushold Family of Three": "Maushold Three",
-    
-    "Palafin Zero Form": "Palafin",
-    
-    "Zygarde 50% Forme": "Zygarde",
-    
-    "Necrozma Ultra Necrozma": "Necrozma Ultra",
-    "Necrozma Dusk Mane Necrozma": "Necrozma Dusk Mane",
-    "Necrozma Dawn Wings Necrozma": "Necrozma Dawn Wings",
-    
-    "Ho-oh": "Ho-Oh",
-    
-    "Darmanitan Standard Mode": "Darmanitan",
-    "Galarian Standard Mode": "Galarian Darmanitan",
-    "Galarian Zen Mode": "Galarian Darmanitan Zen Mode",
-    
-} 
-
-# Apply the replacement rules to the "Name" column
-df["Name"] = df["Name"].replace(replacement_rules)
-
-#Remove Some pokemon from df 
-df = df[~df['Name'].isin(['Pumpkaboo Small Size', 'Pumpkaboo Large Size', 'Pumpkaboo Super Size', 'Gourgeist Small Size', 'Gourgeist Large Size', 'Gourgeist Super Size', 'Rockruff Own Tempo Rockruff', 'Eternatus Eternamax','Partner Eevee','Giratina Altered Forme'])]
-
-#Ensure pokemon of first form comes first in DataFrame. Important for hyperlinks
-def sort_group(group):
-    return group.sort_values(by='Name', key=lambda x: x.str.len())
-df = df.groupby('NDex', group_keys=False).apply(sort_group)
-
-#Make all NDex numbers have 4 digits.
-df['NDex'] = df['NDex'].astype(str).str.zfill(4)
 
 
